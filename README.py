be4fbd6349908b34dccb7d8af79c9f4e2d23145f
dankrainbow <79637386+dankrainbow@users.noreply.github.com>
2021-07-28 14:33:21 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,2 @@
-![Dankrainbows's GitHub stats](https://github-readme-stats.vercel.app/api?username=dankrainbow&show_icons=true&theme=radical)
+
+[<img align="left" alt="Stats" src="https://github-readme-stats.vercel.app/api?username=dankrainbow&show_icons=true&theme=radical" />][website]
